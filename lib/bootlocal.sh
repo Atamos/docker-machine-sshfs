6a56a623125ca2bd79df5ed7c1a8b8d6ca6090fa
marcosciatta <m.sciatta@themadbox.com>
2017-06-05 12:37:59 +0200

    Update bootlocal.sh

--- lib/bootlocal.sh
+++ lib/bootlocal.sh
@@ -7,5 +7,6 @@ chown docker:staff /Users
 su - docker -c  'tce-load -i /var/lib/boot2docker/sshfs-fuse-2.5.x86_64.tcz'
 sudo sh -c "echo 'user_allow_other' > /etc/fuse.conf"
 su - docker -c 'sh -c "sshfs {SSHFS_OPTIONS} -o uid={SSHFS_UID} -o gid={SSHFS_GUID} {USER}@{VBOXIP}:/Users/ /Users/"'
+sysctl -w vm.max_map_count=262144
 echo 'ready'
 
